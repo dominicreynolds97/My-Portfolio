1ac1936ca44093852cd4b152d0e57f697a021391
domr97 <dominicrjreynolds@gmail.com>
2021-07-20 17:28:51 +0100

    more changes

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -7,7 +7,10 @@ export default function Brand() {
       <div className="half">
         <h4>About Me</h4>
         <p>
-          I am a creative problem solver, always finding solutions quickly and inventively to any challenge that I am faced with. My logical thinking always assists and aids with my approach to brain teasers, be it a coding problem or an everyday issue. I decided to make the switch to becoming a software developer and began teaching myself HTML, CSS and JavaScript. When I realised that I was picking them up quickly, while gaining great satisfaction and enjoyment, I knew that this was the right path for me. I decided to fast track my learning with the Software Engineering Immersive course at General Assembly, through this I have gained valuable skills and knowledge over a short space of time. I find programming to be thoroughly enjoyable and it comes naturally to me.
+          I am a creative problem solver, always finding solutions quickly and inventively to any challenge that I am faced with. My logical thinking assists and aids with my approach to brain teasers, be it a coding problem or an everyday issue. 
+        </p>
+        <p>
+          After seeing how much my older brother enjoys his job as a developer, I decided to make the switch to becoming a software developer and began teaching myself HTML, CSS and JavaScript. When I realised that I was gaining great satisfaction and enjoyment from coding, I knew that this was the right path for me. I decided to fast track my learning with the Software Engineering Immersive course at General Assembly, through this I have gained valuable skills and knowledge over a short space of time. I’m looking for a role in which I can continue to learn and develop my skills as a developer, whilst always bringing a good attitude and a strong work ethic. Being able to expand my skills and knowledge is of paramount importance to me moving forward
         </p>
       </div>
       <div className="half">
